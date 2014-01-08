f3f78ac25f1e39c49c12b9f10af0b4e97613cd0d
ttoppanda <coolpanda1219@outlook.com>
2014-01-07 20:29:43 -0500

    Table css

--- view/index.php
+++ view/index.php
@@ -1,24 +1,22 @@
 <h2>Listado de usuarios</h2>
 <table>
-	<tr>
-		<th>Id</th>
-		<th>Nombre</th>
-		<th>Email</th>
-	</tr>
-	<?php
-		foreach ($this->users as  $value) {
-			echo '<tr>';
-				echo '<td>';echo $value['id'];echo '</td>';
-				echo '<td>';echo $value['nombre'];echo '</td>';
-				echo '<td>';echo $value['email'];echo '</td>';
-				echo '<td>';
-					echo '<button data-id="'. $value['id'] .'">Actualizar</button>';
-				echo '</td>';
-				echo '<td>';
-					echo '<button data-id="'. $value['id'] .'">Eliminar</button>';
-				echo '</td>';
-			echo '</tr>';
-		}		
-		
-	?>
+     <caption>Usuarios</caption>
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Nombre</th>
+            <th>Email</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        foreach ($this->users as $value) {
+            echo '<tr><td>' . $value['id'] . '</td>';
+            echo '<td>' . $value['nombre'] . '</td>';
+            echo '<td>' . $value['email'] . '</td>';
+            echo '<td> <button data-id="' . $value['id'] . '">Actualizar</button></td>';
+            echo '<td><button data-id="' . $value['id'] . '">Eliminar</button></td></tr>';
+        }
+        ?>
+    </tbody>    
 </table>
