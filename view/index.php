a002f78d2480f507f7b3a4e6cc0d070d3569a5ce
ttoppanda <coolpanda1219@outlook.com>
2014-01-08 11:42:25 -0500

    popUp Actulizacion

--- view/index.php
+++ view/index.php
@@ -1,6 +1,6 @@
 <h2>Listado de usuarios</h2>
 <table summary="Usuario ">
-     <caption>Usuarios</caption>
+    <caption>Usuarios</caption>
     <thead>
         <tr>
             <th>Id</th>
@@ -16,9 +16,18 @@
             echo '<tr><td>' . $value['id'] . '</td>';
             echo '<td>' . $value['nombre'] . '</td>';
             echo '<td>' . $value['email'] . '</td>';
-            echo '<td> <button data-id="' . $value['id'] . '">Actualizar</button></td>';
+            echo '<td> <button data-id="' . $value['id'] . '" class="update_btn">Actualizar</button></td>';
             echo '<td><button data-id="' . $value['id'] . '">Eliminar</button></td></tr>';
         }
         ?>
     </tbody>    
 </table>
+<div id="container_pop_up"class="container_popup">
+    <form method="post">
+        <label class="input_label">Nombre</label>                
+        <input name="nombre" placeholder="nombre" type="text" value="" maxlength="50"  required/>
+        <label class="input_label">Nombre</label>                
+        <input name="nombre" placeholder="nombre" type="text" value="" maxlength="50"  required/>
+        <input class="btn btn-submit" id="submit" type="submit" value="Enviar"/>
+    </form>
+</div>
--- view/js/script.js
+++ view/js/script.js
@@ -1,4 +1,10 @@
 (function($) {
-
-	alert("jajaj");
+    var $update = $('.update_btn'),
+        $containerPopUp = $('.container_popup');
+    
+    $update.on('click', function(){
+        $containerPopUp.addClass("show");
+        //alert($(this).data("id"));
+    });
+	
 })(jQuery);
